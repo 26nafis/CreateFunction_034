047162ee80f2adee61e65242e24834326b949e51
26nafis <spalanzaninafis@gmail.com>
2025-10-16 16:22:21 +0700

    mebenerkan

--- prak 2 soal 2.py
+++ prak 2 soal 2.py
@@ -2,7 +2,7 @@ lambda jarijari : 3.14159 * jarijari
 
 try :
     jari =float(input("isikan nilai jari - jari : "))
-    result = luas (jari)
+    result = luas(jari)
     print(f"luas dari lingkaran dengan jari-jari {jari} adalah {result}")
 except ValueError :
     print("nilai jari-jari harus angka")
